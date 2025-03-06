ebf4e52ae31dd163fde057e2585746271a1c93ce
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-03-06 10:22:27 -0500

    Update messenger.h

--- messenger.h
+++ messenger.h
@@ -1,5 +1,19 @@
 #ifndef messenger_h
 #define messenger_h
 
+#include <SSLClient.h>
+#include <UrlEncode.h>
+
+#include "trust_anchors.h"
+
+#define SERVER "api.callmebot.com"
+
+// +international_country_code + phone number
+// Portugal +351, example: +351912345678
+// TODO:
+String phoneNumber = "REPLACE_WITH_YOUR_PHONE_NUMBER";
+String apiKey = "REPLACE_WITH_API_KEY";
+String message = "Test from M5 ESP32";
+
 
 #endif

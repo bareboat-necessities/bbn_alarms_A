a4f5195cb6c7bd776c875bd3a89f049c7380e384
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-03-09 23:45:21 -0400

    Update messenger.h

--- messenger.h
+++ messenger.h
@@ -9,7 +9,26 @@
 // +international_country_code + phone number
 // Portugal +351, example: +351912345678
 // TODO:
-String phoneNumber = "REPLACE_WITH_YOUR_PHONE_NUMBER";
-String apiKey = "REPLACE_WITH_API_KEY";
+String phoneNumber = "17326198507"; //"REPLACE_WITH_YOUR_PHONE_NUMBER";
+String apiKey = "6749268"; // "REPLACE_WITH_API_KEY";
+
+void messenger_send(Unit_PoESP32 *eth, String message) {
+  auto resp = eth->createSSLClient(MESSENGER_SERVER, 443);
+  int idx = resp.indexOf("CONNECT");
+  Serial.printf("idx %d\n", idx);
+  if (idx != -1 && idx > 1 && idx < 1024) {
+    int connectionId = resp.charAt(idx - 2) - '0';  // Get the connection ID
+    Serial.print("Established connection ID: ");
+    Serial.println(connectionId);
+
+    String req = String("GET ") + "/whatsapp.php?phone=" + phoneNumber
+                 + "&apikey=" + apiKey + "&text=" + urlEncode(message) + " HTTP/1.1\nHost: " + MESSENGER_SERVER 
+                 + "\nConnection: close\n\n";
+    eth->sendCMD("AT+CIPSEND=" + String(connectionId) + "," + String(req.length()));
+    delay(100);
+    eth->sendCMD(req);
+    delay(2000);
+  }
+}
 
 #endif

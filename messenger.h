0f61d140735121190e6f132014812c4d41f62cee
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-03-10 09:40:44 -0400

    Update messenger.h

--- messenger.h
+++ messenger.h
@@ -4,6 +4,8 @@
 #include <SSLClient.h>
 #include <UrlEncode.h>
 
+#include "Nmea0183Msg.h"
+
 #define MESSENGER_SERVER "api.callmebot.com"
 
 // +international_country_code + phone number
@@ -15,12 +17,12 @@ String apiKey = "REPLACE_WITH_API_KEY";
 void messenger_send(Unit_PoESP32 *eth, String phoneNumber, String apiKey, String message) {
   auto resp = eth->createSSLClient(MESSENGER_SERVER, 443);
   int idx = resp.indexOf("CONNECT");
-  Serial.printf("idx %d\n", idx);
+
   if (idx != -1 && idx > 1 && idx < 1024) {
     int connectionId = resp.charAt(idx - 2) - '0';  // Get the connection ID
-    Serial.print("Established connection ID: ");
-    Serial.println(connectionId);
 
+    gen_nmea0183_msg("$BBTXT,01,01,01,Established connID: %s", String(connectionId).c_str());
+    
     String req = String("GET ") + "/whatsapp.php?phone=" + phoneNumber
                  + "&apikey=" + apiKey + "&text=" + urlEncode(message) + " HTTP/1.1\nHost: " + MESSENGER_SERVER 
                  + "\nConnection: close\n\n";

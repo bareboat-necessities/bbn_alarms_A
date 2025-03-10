096fe4eaf65e15053ed370e675398b238e606ced
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-03-10 10:39:53 -0400

    Update conf_store.h

--- conf_store.h
+++ conf_store.h
@@ -1,4 +1,24 @@
 #ifndef conf_store_h
 #define conf_store_h
 
+#include <Preferences.h>
+
+// config store.
+Preferences preferences;
+
+void save_settings(String phone, String apiKey) {
+  preferences.end();
+  preferences.begin("msg-cfg", false);
+  preferences.putString("phone", phone);
+  preferences.putString("apiKey", apiKey);
+  preferences.end();
+}
+
+void restore_settings() {
+  preferences.begin("msg-cfg", false);
+  phoneNumber = preferences.getString("phone");
+  apiKey = preferences.getString("apiKey");
+  preferences.end();
+}
+
 #endif

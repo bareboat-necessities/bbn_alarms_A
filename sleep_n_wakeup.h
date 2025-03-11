7b967b3d1978245ef7ad95b6539d67f9d70308ea
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-03-11 08:20:05 -0400

    Update sleep_n_wakeup.h

--- sleep_n_wakeup.h
+++ sleep_n_wakeup.h
@@ -1,5 +1,13 @@
 #ifndef sleep_n_wakeup_h
 #define sleep_n_wakeup_h
 
-#endif
+#define uS_TO_S_FACTOR 1000000
 
+#define SLEEP_DURATION (15 * 60 * uS_TO_S_FACTOR)
+
+void cat_nap() {
+  esp_sleep_enable_timer_wakeup(SLEEP_DURATION);
+  esp_deep_sleep_start();
+}
+
+#endif

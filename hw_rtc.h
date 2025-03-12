62d2ae99e451c2d013bb854e0a9e9072744ce43c
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-03-12 13:57:39 -0400

    Update hw_rtc.h

--- hw_rtc.h
+++ hw_rtc.h
@@ -12,6 +12,18 @@ extern "C" {
     M5.Rtc.begin();
   }
 
+  void rtc_set(struct tm *timeinfo) {
+    RTCdate.Year = timeinfo->tm_year;
+    RTCdate.Month = timeinfo->tm_mon;
+    RTCdate.Date = timeinfo->tm_mday;
+    M5.Rtc.SetDate(&RTCdate);
+
+    RTCtime.Hours = timeinfo->tm_hour;
+    RTCtime.Minutes = timeinfo->tm_min;
+    RTCtime.Seconds = timeinfo->tm_sec;
+    M5.Rtc.SetTime(&RTCtime);  // writes the  time to the (RTC) real time clock.
+  }
+
 #ifdef __cplusplus
 } /*extern "C"*/
 #endif

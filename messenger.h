757892a9f034df130ffaf19c26c9d5c73c00228b
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-03-13 03:32:53 -0400

    Update messenger.h

--- messenger.h
+++ messenger.h
@@ -28,7 +28,7 @@ void messenger_send(Unit_PoESP32 *eth, String phoneNumber, String apiKey, String
                    + "\r\nConnection: close\r\n\r\n";
       
       eth->sendTCPString(connectionId, req.c_str());
-      delay(15000);
+      delay(2000);
     }
   }
 }

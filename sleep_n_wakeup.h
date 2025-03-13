eb682694431282de51d23bb1c815cc563a37f381
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-03-13 01:54:41 -0400

    Update sleep_n_wakeup.h

--- sleep_n_wakeup.h
+++ sleep_n_wakeup.h
@@ -3,7 +3,7 @@
 
 #define uS_TO_S_FACTOR 1000000
 
-#define SLEEP_DURATION (5 * 60 * uS_TO_S_FACTOR)
+#define SLEEP_DURATION (15 * 60 * uS_TO_S_FACTOR)
 
 void cat_nap(unsigned long sleep_uS = SLEEP_DURATION) {
   esp_sleep_enable_timer_wakeup(sleep_uS);

3075dfe3431e18a69770b2893508e8207633be4a
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2025-03-17 14:07:13 -0400

    Update bbn_alarms_A.ino

--- bbn_alarms_A.ino
+++ bbn_alarms_A.ino
@@ -159,7 +159,7 @@ void loop() {
 
     // Check if a new client has connected
     int idx = response.indexOf("+IPD");
-    if (idx != -1 && idx < 1024) {
+    if (idx != -1 && idx < 6144) {
       // Extract the connection ID and data
       int connectionId = response.charAt(5 + idx) - '0';  // Get the connection ID
       String httpRequest = response.substring(7 + idx);   // Get the data
